bb03eee6002160fe5b3baa93cbf96565cc0430a1
Maksim Bannikov <spystieber@gmail.com>
2024-06-03 17:38:13 +0300

    Init

---
+++ Brewfile
@@ -0,0 +1,50 @@
+tap "homebrew/bundle"
+tap "homebrew/services"
+# Clone of cat(1) with syntax highlighting and Git integration
+brew "bat"
+# GNU File, Shell, and Text utilities
+brew "coreutils"
+# Get a file from an HTTP, HTTPS or FTP server
+brew "curl"
+# Distributed revision control system
+brew "git"
+# Mac App Store command-line interface
+brew "mas"
+# Convert source code to formatted text with syntax highlighting
+brew "highlight"
+# Lightweight and flexible command-line JSON processor
+brew "jq"
+# File browser
+brew "ranger"
+# Command-line Interface for Scaleway
+brew "scw"
+# YAML Parser
+brew "libyaml"
+# Python version management
+brew "pyenv"
+# Next-generation plugin manager for zsh
+brew "zplug"
+# Command-line fuzzy finder written in Go
+brew "fzf"
+# UNIX shell (command interpreter)
+brew "zsh"
+# Voice and text chat software
+cask "discord"
+# App to build and share containerised applications and microservices
+cask "docker"
+# Web browser
+cask "firefox"
+# Terminal built on web technologies
+cask "hyper"
+# Support for Logitech G gear
+cask "logitech-g-hub"
+# Quicklook extension for source files
+cask "syntax-highlight"
+# Messaging app with a focus on speed and security
+cask "telegram"
+# Unofficial app for Yandex Music
+cask "yandex-music"
+# Video communication and virtual meeting platform
+cask "zoom"
+mas "HazeOver", id: 430798174
+mas "Delete Apps", id: 1033808943
---
+++ shell/.aliases-docker.sh
@@ -0,0 +1,95 @@
+############################################################################
+#                                                                          #
+#               ------- Useful Docker Aliases --------                     #
+#                                                                          #
+#     # Installation :                                                     #
+#     copy/paste these lines into your .bashrc or .zshrc file or just      #
+#     type the following in your current shell to try it out:              #
+#     wget -O - https://gist.githubusercontent.com/jgrodziski/9ed4a17709baad10dbcd4530b60dfcbb/raw/d84ef1741c59e7ab07fb055a70df1830584c6c18/docker-aliases.sh | bash
+#                                                                          #
+#     # Usage:                                                             #
+#     dcu            : docker-compose up -d                                #
+#     dcd            : docker-compose down                                 #
+#     dex <container>: execute a bash shell inside the RUNNING <container> #
+#     di <container> : docker inspect <container>                          #
+#     dim            : docker images                                       #
+#     dip            : IP addresses of all running containers              #
+#     dl <container> : docker logs -f <container>                          #
+#     dnames         : names of all running containers                     #
+#     dps            : docker ps                                           #
+#     dpsa           : docker ps -a                                        #
+#     drmc           : remove all exited containers                        #
+#     drmid          : remove all dangling images                          #
+#     drun <image>   : execute a bash shell in NEW container from <image>  #
+#     dsr <container>: stop then remove <container>                        #
+#                                                                          #
+############################################################################
+
+function dnames-fn {
+	for ID in `docker ps | awk '{print $1}' | grep -v 'CONTAINER'`
+	do
+    	docker inspect $ID | grep Name | head -1 | awk '{print $2}' | sed 's/,//g' | sed 's%/%%g' | sed 's/"//g'
+	done
+}
+
+function dip-fn {
+	echo "IP addresses of all named running containers"
+
+	for DOC in `dnames-fn`
+	do
+  		IP=`docker inspect $DOC | grep -m3 IPAddress | cut -d '"' -f 4 | tr -d "\n"`
+  		echo $DOC : $IP
+	done
+}
+
+function dex-fn {
+    docker exec -it $1 ${2:-bash}
+}
+
+function di-fn {
+	docker inspect $1
+}
+
+function dl-fn {
+	docker logs -f $1
+}
+
+function drun-fn {
+    docker run -it $1 $2
+}
+
+function dsr-fn {
+	docker stop $1;docker rm $1
+}
+
+function drmc-fn {
+       docker rm $(docker ps --all -q -f status=exited)
+}
+
+function drmid-fn {
+       imgs=$(docker images -q -f dangling=true)
+       [ ! -z "$imgs" ] && docker rmi "$imgs" || echo "no dangling images."
+}
+
+# in order to do things like dex $(dlab label) sh
+function dlab {
+       docker ps --filter="label=$1" --format="{{.ID}}"
+}
+
+alias dcu="docker compose up -d"
+alias dcd="docker compose down"
+alias dcb="docker compose build"
+alias dcr="dcd && dcu"
+alias dex=dex-fn
+alias di=di-fn
+alias dim="docker images"
+alias dip=dip-fn
+alias dl=dl-fn
+alias dnames=dnames-fn
+alias dps="docker ps"
+alias dpsa="docker ps -a"
+alias drmc=drmc-fn
+alias drmid=drmid-fn
+alias drun=drun-fn
+alias dsr=dsr-fn
+
